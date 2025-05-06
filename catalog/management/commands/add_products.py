baed061ef691ed42714e28ef14cb4d720fd7def2
Ksenia Kovaleva <kovalevaksenia21@gmail.com>
2025-05-07 00:53:05 +0200

    homework_23 добавлены новые скриншоты и доработана кастомная команда

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-
+from catalog.models import Category, Product
 from django.core.management import call_command
 
 
@@ -7,6 +7,9 @@ class Command(BaseCommand):
     help = 'Load test data from fixture'
 
     def handle(self, *args, **kwargs):
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+
         call_command('loaddata', 'category_fixture.json')
         call_command('loaddata', 'product_fixture.json')
 
